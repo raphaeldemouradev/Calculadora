b96e3a174b5bbcfb632320190a4eb9cf793a184c
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-04-06 21:56:56 -0300

    calculadora adicionada

---
+++ script.js
@@ -0,0 +1,12 @@
+function numero(num) {
+    var numeros = document.getElementById('res').innerHTML
+    res.innerHTML = numeros + num
+}
+
+function igual() {                                     
+    res.innerHTML = `Hello Word!`
+}
+
+function apagar() {
+    res.innerHTML = ''
+}
